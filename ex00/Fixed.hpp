e576ea58baf9343ea6bd2737758600d8f601e54c
Alvaro Martin Falagan <alvamart@c1r16s3.42madrid.com>
2025-02-26 17:25:27 +0100

    FIn

---
+++ ex00/Fixed.hpp
@@ -0,0 +1,18 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+class Fixed
+{
+	public:
+		Fixed();
+		Fixed(const Fixed &f);
+		Fixed &operator=(const Fixed &f);
+		~Fixed();
+		int getRawBits(void) const;
+		void setRawBits( int const raw );
+	private:
+		    int					value;
+    		static const int	fracBits = 8;
+};
+
+#endif
