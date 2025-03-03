790be82b5bae63163e2e26820ec88e73237290b1
Alvaro297 <alvaromartinfalagan29@gmail.com>
2025-03-03 17:05:17 +0100

    Ejer00 terminado

---
+++ ex00/Fixed.cpp
@@ -0,0 +1,19 @@
+#include "Fixed.hpp"
+
+Fixed &Fixed::operator=(const Fixed &f)
+{
+	if (this != &f)
+		this->value = f.value;
+	return (*this);
+}
+
+Fixed::Fixed(const Fixed &f)
+{
+	this->value = f.value;
+}
+Fixed::Fixed(){}
+
+Fixed::~Fixed(){}
+
+int Fixed::getRawBits(void) const { return this->value; }
+void Fixed::setRawBits(int const raw){ this->value = raw * (2^this->fracBits);}
--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -11,8 +11,8 @@ class Fixed
 		int getRawBits(void) const;
 		void setRawBits( int const raw );
 	private:
-		    int					value;
-    		static const int	fracBits = 8;
+			int					value;
+			static const int	fracBits = 8;
 };
 
 #endif
